109b5e71f9cd567856905db3838d95869c8a0e9d
Diana Müller <diana.muller@globalhitss.com.br>
2024-06-13 11:25:27 -0300

    overlay and position of dialog

--- space-app/src/components/ModalZoom/index.jsx
+++ space-app/src/components/ModalZoom/index.jsx
@@ -1,15 +1,20 @@
-import { PhotoImage } from "../Gallery/PhotoImage"
+import { PhotoImage } from "../Gallery/PhotoImage";
+import * as S from "./style.js";
 
-export const ModalZoom = ({photo}) => {
-    return (
-        <>
-        {/* !! transforma objeto em boleano, se for true vira false, se for true vira false */}
-            {photo && <dialog open={!!photo}>
-                <PhotoImage photo={photo} isExtended={true}/>
-                <form method="dialog">
-                    <button>ok</button>
-                </form>
-            </dialog>}
-        </>
-    )
-}
+export const ModalZoom = ({ photo }) => {
+  return (
+    <>
+      {/* !! transforma objeto em boleano, se for true vira false, se for true vira false */}
+      {photo && (
+        <S.Overlay>
+          <S.Dialog open={!!photo}>
+            <PhotoImage photo={photo} isExtended={true} />
+            <form method="dialog">
+              <button>ok</button>
+            </form>
+          </S.Dialog>
+        </S.Overlay>
+      )}
+    </>
+  );
+};
---
+++ space-app/src/components/ModalZoom/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+export const Dialog = styled.dialog`
+  position: absolute;
+  top: 294px;
+`;
